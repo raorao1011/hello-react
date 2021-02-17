3734b36a7ada966bb15202136c84a3e4174d4471
RAO <raorao1011@gmail.com>
2021-02-17 17:59:53 +0900

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Counter from "./components/Counter";
+
 const App = () => {
     return (
         <>
             <div>App</div>
+            <Counter />
         </>
     );
 };
+
+export default App;
