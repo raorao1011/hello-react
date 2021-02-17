4410fcf5b4547237cea6fead80932225338838f1
RAO <raorao1011@gmail.com>
2021-02-17 17:53:47 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-import Button from "./Button";
-
 const App = () => {
     return (
         <>
             <div>App</div>
-            <Button title="post"/>
-            <Button title="edit"/>
-            <Button title="delete"/>
-            <Button title="submit"/>        </>
+        </>
     );
 };
-
-export default App;
