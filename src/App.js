a04a4f82a7c2181dbb2ea4e1dd8f9ab8197e64ef
RAO <raorao1011@gmail.com>
2021-02-17 17:39:51 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import Button from "./Button";
+
 const App = () => {
     return (
         <>
             <div>App</div>
-        </>
+            <Button title="post"/>
+            <Button title="edit"/>
+            <Button title="delete"/>
+            <Button title="submit"/>        </>
     );
 };
 
